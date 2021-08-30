d5f6e47165b78cfc25f8a68317c1ebc652d43651
beastsunny <beastsunny06@gmail.com>
2021-08-30 15:49:04 +0530

    feat: dob sum function

---
+++ script.js
@@ -0,0 +1,32 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-now");
+
+
+function checkBirthDateIsLucky() {
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
+}
+
+
+function calculateSum(dob) {
+    dob = dob.replaceAll("-", "");
+    let sum = 0;
+    for (let i = 0; i < dob.length; i++) {
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
+
+}
+
+
+
+
+
+
+
+
+
+
+checkNumberButton.addEventListener('click', checkBirthDateIsLucky)
